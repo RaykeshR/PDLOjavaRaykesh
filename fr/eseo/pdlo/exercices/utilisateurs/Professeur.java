74a291e26555459cedce45c341a39c0860e5cb1f
Raykesh <102523616+RaykeshR@users.noreply.github.com>
2024-03-04 20:14:07 +0100

    Add files via upload

--- fr/eseo/pdlo/exercices/utilisateurs/Professeur.java
+++ fr/eseo/pdlo/exercices/utilisateurs/Professeur.java
@@ -2,6 +2,7 @@ package fr.eseo.pdlo.exercices.utilisateurs;
 import java.util.ArrayList;
 
 public class Professeur extends Personne{
+	static final String NATIONALITE_PAR_DEFAUT = "française";
 	private ArrayList<String> cours ;
 	public Professeur(String prenom, String nom, String nationalite, int anneeDeNaissance){
         //this . nom = nom;
@@ -12,7 +13,10 @@ public class Professeur extends Personne{
 		super(prenom,nom,nationalite,anneeDeNaissance);
         cours =new ArrayList<String>();
 	}
-	public Professeur(String prenom, String nom){cours =new ArrayList<String>();}
+	public Professeur(String prenom, String nom){
+		super(prenom,nom,NATIONALITE_PAR_DEFAUT,0000);
+		cours =new ArrayList<String>();
+	}
 	public void ajouterCours (String cours) {this.cours.add(cours);}
 	//public ArrayList<Personne> getCours() {return L;}
 	public ArrayList<String> getCours() {return cours;}
